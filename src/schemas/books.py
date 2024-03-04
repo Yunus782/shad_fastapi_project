40e3214697f1d2ed1c8cb179d74973bd82c77030
asadulaev-yu <asadulaev.yu@yandex.ru>
2024-03-04 15:26:25 +0300

    delete the seller_id field from the json of the seller's books for the endpoint seller/{id}

--- src/schemas/books.py
+++ src/schemas/books.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, field_validator
 from pydantic_core import PydanticCustomError
 
-__all__ = ["IncomingBook", "ReturnedAllBooks", "ReturnedBook"]
+__all__ = ["IncomingBook", "ReturnedAllBooks", "ReturnedBook", "SellerBook"]
 
 
 # Базовый класс "Книги", содержащий поля, которые есть во всех классах-наследниках.
@@ -37,3 +37,15 @@ class ReturnedBook(BaseBook):
 # Класс для возврата массива объектов "Книга"
 class ReturnedAllBooks(BaseModel):
     books: list[ReturnedBook]
+
+
+class SellerBook(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: int
+    count_pages: int
+
+    class Config:
+        orm_mode = True
+        exclude = {"seller_id"}
--- src/schemas/sellers.py
+++ src/schemas/sellers.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel, field_validator, EmailStr, Field
 from passlib.context import CryptContext
 from pydantic_core import PydanticCustomError
 
-from src.schemas import ReturnedBook
+from src.schemas.books import SellerBook
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -38,7 +39,10 @@ class ReturnedSeller(BaseSeller):
 
 
 class ReturnedSellersDetails(ReturnedSeller):
-    books: list[ReturnedBook]
+    books: list[SellerBook]
+
+    class Config:
+        orm_mode = True
 
 
 class ReturnedAllSellers(BaseModel):
@@ -49,6 +53,7 @@ class SellerUpdate(BaseModel):
     first_name: str = Field(None, min_length=2, max_length=50)
     last_name: str = Field(None, min_length=2, max_length=50)
     email: EmailStr = None
+
     @field_validator("first_name", "last_name")
     @staticmethod
     def validate_name_fields(value: str, field: str):
